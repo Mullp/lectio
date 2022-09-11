1d57701aa5d8b52a608a70d4749bfc27244fc157
Mullp <mullp@protonmail.com>
2022-09-11 21:27:37 +0200

    Added dotenv

--- src/__tests__/classes/Client.test.ts
+++ src/__tests__/classes/Client.test.ts
@@ -1,6 +1,10 @@
 import { Client } from "../../index";
 
-const lectio = new Client({ schoolId: "", username: "", password: "" });
+const lectio = new Client({
+  schoolId: process.env.LECTIO_SCHOOLID as string,
+  username: process.env.LECTIO_USERNAME as string,
+  password: process.env.LECTIO_PASSWORD as string,
+});
 
 test("Sign in", async () => {
   expect(await lectio.authenticate()).toBeTruthy();
